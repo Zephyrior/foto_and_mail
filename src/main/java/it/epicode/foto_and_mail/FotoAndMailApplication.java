070f582d8d6d5a14bb74a7badd1f84cca76e72d2
Zephyrior <mark.temblique@gmail.com>
2025-04-17 10:13:15 +0200

    Initial commit

---
+++ src/main/java/it/epicode/foto_and_mail/FotoAndMailApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.foto_and_mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotoAndMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotoAndMailApplication.class, args);
+	}
+
+}
